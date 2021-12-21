6831c73408c5bc5a350dfc16d5fa12f89e9f2894
Jeff Soules <jsoules@flatironinstitute.org>
2021-12-21 09:57:06 -0500

    Add three-point light functionality to Scene3DPanelView.

--- gui/src/FourPanelView/Scene3DPanelView/Scene3DPanelView.tsx
+++ gui/src/FourPanelView/Scene3DPanelView/Scene3DPanelView.tsx
@@ -15,6 +15,26 @@ type Props = {
     height: number
 }
 
+const addThreePointLights = (camera: THREE.PerspectiveCamera, test: boolean = false) => {
+    // A traditional three-light setup has the main shadow-generating light, or key light,
+    // slightly above the camera and at an angle ~30-45 degrees to the subject;
+    // the fill light comes from the other side and is softer and is trying to ensure shadows aren't
+    // too dramatic;
+    // then a rim light opposite the camera provides additional highlights to the object edges.
+    // The key light should be the brightest light source.
+    // Adding these to the camera ensures they'll always be relative to the camera position and
+    // use its coordinate system.
+    const colors = test ? [0xff0000, 0x0000ff, 0x00ff00] : [0xffffff, 0xffffff, 0xffffff]
+    const keyLight = new THREE.SpotLight(colors[0], .60)
+    const fillLight = new THREE.DirectionalLight(colors[1], .4)
+    const rimLight = new THREE.DirectionalLight(colors[2], .3)
+    ;[keyLight, fillLight, rimLight].forEach(l => camera.add(l))
+    keyLight.position.set(7, 2, 0)
+    fillLight.position.set(-7, 2, 0)
+    rimLight.position.set(0, 1, -2)
+    rimLight.target = camera
+}
+
 const planeMesh = (p0: [number, number, number], v1: [number, number, number], v2: [number, number, number], normal: [number, number, number], color: string, opts: {transparent: boolean, opacity: number}) => {
     const material = new THREE.MeshPhongMaterial( {
         color,
@@ -177,22 +197,14 @@ const Scene3DPanelView: FunctionComponent<Props> = ({grid, focusPosition, surfac
         while (container.firstChild) container.removeChild(container.firstChild)
         container.appendChild(renderer.domElement)
         
-        // var cube = new THREE.Mesh( geometry, material );
-        // scene.add( cube );
+        objects.forEach(obj => scene.add(obj))
 
-        // const mesh = new THREE.Mesh( getMeshGeometry(), material );
-        // scene.add(mesh)
-
-        for (let obj of objects) {
-            scene.add(obj)
-        }
-
-        // const light = new THREE.HemisphereLight();
-        const lights = [new THREE.PointLight(0xffffff, .5), new THREE.AmbientLight(0xffffff, 0.2)]
-        lights.forEach(l => scene.add(l))
+        // const lights = [new THREE.PointLight(0xffffff, .5), new THREE.AmbientLight(0xffffff, 0.2)]
+        // lights.forEach(l => camera.add(l))
+        addThreePointLights(camera)
+        scene.add(camera)
 
         const render = () => {
-            lights[0].position.set(camera.position.x, camera.position.y, camera.position.z)
             renderer.render( scene, camera );
         }
         controls.addEventListener( 'change', render );
