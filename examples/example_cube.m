07817bca5717b76d60a1f1002317294c42760336
Jeremy Magland <jeremy.magland@gmail.com>
2022-01-28 09:19:47 -0500

    add matlab example

---
+++ examples/example_cube.m
@@ -0,0 +1,34 @@
+% Important: you must run the following prior to executing this script.
+% Otherwise, MATLAB will likely crash due to Python library conflicts.
+%
+% pyenv("ExecutionMode", "OutOfProcess")
+
+% define vertices and faces of a cube
+a = 30;
+vertices = [[-a, -a, -a]; [-a, -a, a]; [-a, a, -a]; [-a, a, a]; [a, -a, -a]; [a, -a, a]; [a, a, -a]; [a, a, a]];
+faces = [
+    [0, 1, 3]; [0, 3, 2]; [4, 5, 7]; [4, 7, 6]; % [-a, *, *] [a, *, *]
+    [0, 1, 5]; [0, 5, 4]; [2, 3, 7]; [2, 7, 6]; % [*, -a, *] [*, a, *]
+    [0, 2, 6]; [0, 6, 4]; [1, 3, 7]; [1, 7, 5]  % [*, *, -a] [*, *, a]
+];
+
+% define a scalar field on the vertices of the surface
+scalar1 = [0, 1, 2, 3, 4, 5, 6, 7];
+
+% Create a VolumeView workspace and add a surface and scalar field
+W = py.volumeview.Workspace();
+surf = W.add_surface(pyargs( ...
+    'name', 'cube', ...
+    'vertices', py.numpy.array(vertices, pyargs('dtype', 'float32')), ...
+    'faces', py.numpy.array(faces, pyargs('dtype', 'int32')) ...
+));
+W.add_surface_scalar_field(pyargs( ...
+    'name', 'scalar1', ...
+    'surface', surf, ...
+    'data', py.numpy.array(scalar1, pyargs('dtype', 'float32')) ...
+));
+
+% Create and display the figURL
+F = W.create_figure();
+url = char(F.url(pyargs('label', 'matlab-test')));
+disp(sprintf('<a href="%s">%s</a>', url, url))
